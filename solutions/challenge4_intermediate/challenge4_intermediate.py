13a7ddee5251c536cd54509022f7b519e7b7a8f0
luke-demas <77931049+luke-demas@users.noreply.github.com>
2021-05-27 21:14:07 -0400

    Completed [intermediate] challenge #4
    Solution added for [easy] challenge #4 along with 'extra credit'

---
+++ solutions/challenge4_intermediate/challenge4_intermediate.py
@@ -0,0 +1,11 @@
+# r/dailyprogrammer
+# challenge 4, intermediate
+# https://www.reddit.com/r/dailyprogrammer/comments/pm6sq/2122012_challenge_4_intermediate/
+
+# solution by Luke Demas
+
+# Basic Math Calculator
+
+import math
+
+print(eval(input("What would you like to calculate?\n")))
